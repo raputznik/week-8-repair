a98859a97c2aa84563ab3cf19ffa1069ebcfffe7
Ninjaparade <yazjallad@gmail.com>
2013-06-21 14:05:45 -0700

    Updated READ.ME and added project files.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ week-8-repair
 =============
 
 Fix the 10 issues with this project.
+
+
+1) Fork this project.
+2) Clone the forked copy of the project to your Applications/MAMP/htdocs folder
+(Like we did for week 8)
+3) Launch MAMP, turn on your servers, (Both green lights should be on)
+4) Open your browser launch the index.html file.
+5) find the errors and fix. Commit with details of fixed after EACH issue you find.
+
+We will go over the assignment in class Friday.
---
+++ js/application.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function($) {
+	$('submit').click(function(event) {
+		console.log("Submit button was clicked");
+	});
+	('.form').validate({
+		rules  {
+			username :{
+				required: true,
+				email : true
+			},
+
+			password : {
+				required :true,
+				minlength : 6,
+				maxlength : 10
+			}
+		},
+
+		messages : {
+			username : {
+				required : "Come on don't be shy",
+				email : "Your email looks funny"
+			},
+
+			password :{
+				required :"Enter your password",
+				minlength : jQuery.format("Your password is at least {0} characters long"),
+				maxlength : jQuery.format("Your password can't be longer than {0} characters")
+			
+		}
+
+	});
+});
+
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
